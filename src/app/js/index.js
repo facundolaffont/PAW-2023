fadd54b575bd2e8e5b41279249f8852cb478cdf3
Facundo Laffont <facundol@mac-osx.local>
2023-05-28 19:37:44 -0300

    Achica la relación de aspecto de las imágenes

--- src/app/js/index.js
+++ src/app/js/index.js
@@ -1,9 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
     const images = [
-        'https://picsum.photos/1100/300',
-        'https://picsum.photos/1101/300',
-        'https://picsum.photos/1102/300',
-        'https://picsum.photos/1103/300'
+        'https://fastly.picsum.photos/id/110/600/300.jpg?hmac=le4JQdpq6E8q7_4n0sm6s0u3HCHF2EBXNxNfdX5yFFE',
+        'https://fastly.picsum.photos/id/123/600/300.jpg?hmac=ndRm4nk5U-MKG46TFNkafmvULEKZ1iPAtqj_uQ6T-s4',
+        'https://fastly.picsum.photos/id/820/600/300.jpg?hmac=WduOsSHELvoldEAv95pW9a6HcGe-xJKqsCUdFIWcovM',
+        'https://fastly.picsum.photos/id/1036/600/300.jpg?hmac=ehdcmkMujDc6endZ47O_f0czlJTlCRDDlOmWZO43_h4'
     ];
 
     console.debug("Se cargará el carrusel...");
